bdfb8c1f1e47c0b6640a7d1a593ec27c5f210571
Liang Kun <liangkun@outlook.com>
2014-07-13 21:15:06 +0800

    first commit

---
+++ pelicanconf.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'liangkun\x1b[C\x1bLiang Kun'
+SITENAME = u'Data Intelli'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Shanghai'
+
+DEFAULT_LANG = u'zh'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS = (('我原来的Blog', 'http://freearth.blog.chinaunix.net'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),)
+
+DEFAULT_PAGINATION = 7
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
